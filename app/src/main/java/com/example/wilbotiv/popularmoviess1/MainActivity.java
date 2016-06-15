64fedbf7fe1fb4b7b0509bd0ae6045e81eeb8fa7
wilbotiv <will.cox.iv@gmail.com>
2016-06-15 15:09:55 -0400

    Added Movie Contract

--- app/src/main/java/com/example/wilbotiv/popularmoviess1/MainActivity.java
+++ app/src/main/java/com/example/wilbotiv/popularmoviess1/MainActivity.java
@@ -3,13 +3,16 @@ package com.example.wilbotiv.popularmoviess1;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
 
+    private final static String TAG = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "The onCreate() method has been called");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
@@ -17,6 +20,36 @@ public class MainActivity extends ActionBarActivity {
                     .add(R.id.container, new MoviesFragment())
                     .commit();
         }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "The onStart() method has been called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "The onResume() method has been called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "The onPause() method has been called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "The onStop() method has been called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "The onDestroy() method has been called");
     }
 
     @Override

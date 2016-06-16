d15f766ea2d4c23c58d7b7f136dee64f1d1b83dc
wilbotiv <will.cox.iv@gmail.com>
2016-06-15 20:26:01 -0400

    Still learning GIT. What am I doing here....

--- app/src/main/java/com/example/wilbotiv/popularmoviess1/MovieContract.java
+++ app/src/main/java/com/example/wilbotiv/popularmoviess1/MovieContract.java
@@ -1,5 +1,6 @@
 package com.example.wilbotiv.popularmoviess1;
 
+import android.content.ContentResolver;
 import android.content.ContentUris;
 import android.net.Uri;
 import android.provider.BaseColumns;
@@ -8,20 +9,14 @@ import android.provider.BaseColumns;
  * Created by wilbotiv on 6/6/2016.
  */
 public class MovieContract {
-    public static final String CONTENT_AUTHORITY = "com.example.wilbot.popularmoviess1";
+    public static final String CONTENT_AUTHORITY = "com.example.wilbotiv.popularmoviess1";
     private static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
 
     public static final String PATH_MOVIE = "movie";
-    public static final String PATH_FAVORITE = "favorite";
-//    public static final String PATH_REVIEWS = "movie";
+//    public static final String PATH_FAVORITE =     "favorite";
+//    public static final String PATH_REVIEWS = "reviews";
 
     public static final class MovieEntry implements BaseColumns {
-        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_MOVIE).build();
-
-        public static final String CONTENT_TYPE =
-                "vnd.android.cursor.dir" + CONTENT_URI + "/" + PATH_MOVIE;
-        public static final String CONTENT_ITEM_TYPE =
-                "vnd.android.cursor.item" + CONTENT_URI + "/" + PATH_MOVIE;
 
         public static final String TABLE_NAME = "movieTable";
 
@@ -29,12 +24,30 @@ public class MovieContract {
         public static final String COLUMN_OVERVIEW = "movieOverview";
         public static final String COLUMN_RELEASEDATE = "movieReleaseDate";
         public static final String COLUMN_ORIGINALTITLE = "movieOriginalTitle";
-        public static final String COLUMN_ID = "movieId";
+        public static final String COLUMN_ID_KEY = "movieId";
         public static final String COLUMN_VOTEAVERAGE = "movieVoteAverage";
+
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_MOVIE).build();
+
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_MOVIE;
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_MOVIE;
+
+        public static final String[] PROJECTION_ALL = {_ID, COLUMN_POSTERPATH, COLUMN_OVERVIEW, COLUMN_RELEASEDATE, COLUMN_ORIGINALTITLE, COLUMN_VOTEAVERAGE};
+        public static final String SORT_ORDER_ASC = COLUMN_ORIGINALTITLE + " ASC";
 
         public static Uri buildMovieUri(long id) {
             return ContentUris.withAppendedId(CONTENT_URI, id);
         }
+    }
+
+    public static final class MovieReview implements BaseColumns{
+
+        public static final String TABLE_NAME = "reviewTable";
+
+        public static final String COLUMN_REVIEW = "movieReview";
+        public static final String COLUMN_ID = "movieId";
     }
 }
 
---
+++ app/src/main/java/com/example/wilbotiv/popularmoviess1/MovieDBHelper.java
@@ -0,0 +1,53 @@
+package com.example.wilbotiv.popularmoviess1;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by wilbotiv on 6/6/2016.
+ */
+public class MovieDbHelper extends SQLiteOpenHelper {
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "movies.db";
+
+    public MovieDbHelper(Context context) {
+        super(context,DATABASE_NAME,null,DATABASE_VERSION);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        final String SQL_CREATE_MOVIE_TABLE = "CREATE TABLE" + MovieContract.MovieEntry.TABLE_NAME + " (" +
+                MovieContract.MovieEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                MovieContract.MovieEntry.COLUMN_ORIGINALTITLE + " TEXT NOT NULL, " +
+                MovieContract.MovieEntry.COLUMN_ID_KEY + " TEXT UNIQUE NOT NULL, " +
+                MovieContract.MovieEntry.COLUMN_RELEASEDATE + " TEXT NOT NULL, " +
+                MovieContract.MovieEntry.COLUMN_OVERVIEW + " TEXT NOT NULL, " +
+                MovieContract.MovieEntry.COLUMN_VOTEAVERAGE + " TEXT NOT NULL, " +
+                MovieContract.MovieEntry.COLUMN_POSTERPATH + " TEXT NOT NULL;";
+
+        db.execSQL(SQL_CREATE_MOVIE_TABLE);
+    }
+/*
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        addMovieTable(db);
+    }
+
+    *//*private void addMovieTable(SQLiteDatabase db) {
+        db.execSQL(
+                "CREATE TABLE" + MovieContract.MovieEntry.TABLE_NAME + "(" +
+                        MovieContract.MovieEntry._ID + " INTEGER PRIMARY KEY, " +
+                        MovieContract.MovieEntry.COLUMN_ORIGINALTITLE + " TEXT NOT NULL, " +
+                        MovieContract.MovieEntry.COLUMN_RELEASEDATE + " TEXT NOT NULL, " +
+                        MovieContract.MovieEntry.COLUMN_OVERVIEW + " TEXT NOT NULL, " +
+                        MovieContract.MovieEntry.COLUMN_VOTEAVERAGE + " TEXT NOT NULL, " +
+                        MovieContract.MovieEntry.COLUMN_POSTERPATH + " TEXT NOT NULL "
+        );
+    }*/
+}

dd84e148561d524d74e06fe7eab5ce4718c3f06c
wilbotiv <will.cox.iv@gmail.com>
2016-06-30 14:34:19 -0400

    Working, about to implement Loader.

--- app/src/main/java/com/example/wilbotiv/popularmoviess1/DetailActivity.java
+++ app/src/main/java/com/example/wilbotiv/popularmoviess1/DetailActivity.java
@@ -1,16 +1,25 @@
 package com.example.wilbotiv.popularmoviess1;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.ShareActionProvider;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
 import com.squareup.picasso.Picasso;
 
 public class DetailActivity extends ActionBarActivity {
@@ -21,14 +30,14 @@ public class DetailActivity extends ActionBarActivity {
         setContentView(R.layout.activity_detail);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new DetailFragment())
                     .commit();
         }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-            getMenuInflater().inflate(R.menu.detail, menu);
+        getMenuInflater().inflate(R.menu.detail, menu);
         return true;
     }
 
@@ -52,31 +61,155 @@ public class DetailActivity extends ActionBarActivity {
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment {
+    public static class DetailFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
+        private String mMovieString;
 
-        public PlaceholderFragment() {
+        private static final String LOG_TAG = DetailFragment.class.getSimpleName();
+
+        private static final String MOVIE_SHARE_HASHTAG = " #PopularMovies";
+
+        private ShareActionProvider mShareActionProvider;
+//        private String mForecast;
+
+        private static final int DETAIL_LOADER = 0;
+
+        private static final String[] MOVIE_COLUMNS = {
+                MovieContract.MovieEntry.TABLE_NAME + "." + MovieContract.MovieEntry._ID,
+                MovieContract.MovieEntry.COLUMN_POSTERPATH,
+                MovieContract.MovieEntry.COLUMN_ORIGINALTITLE,
+                MovieContract.MovieEntry.COLUMN_RELEASEDATE,
+                MovieContract.MovieEntry.COLUMN_OVERVIEW,
+                MovieContract.MovieEntry.COLUMN_VOTEAVERAGE,
+                MovieContract.MovieEntry.COLUMN_ID_KEY,
+        };
+
+        // these constants correspond to the projection defined above, and must change if the
+        // projection changes
+        private static final int COL_MOVIE_ID = 0;
+        private static final int COL_POSTERPATH = 1;
+        private static final int COL_ORIGINALTITLE = 2;
+        private static final int COL_RELEASEDATE = 3;
+        private static final int COL_OVERVIEW = 4;
+        private static final int COL_VOTEAVERAGE = 5;
+        private static final int COL_ID_KEY = 6;
+
+        public DetailFragment() {
+            setHasOptionsMenu(true);
         }
 
-        /*@Override
+        @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-
             View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
-            Intent intent = getActivity().getIntent();
-            Movie movie = intent.getParcelableExtra(MoviesFragment.PAR_KEY);
-            TextView textViewOriginalTitle = (TextView) rootView.findViewById(R.id.fragment_detail_textView_originalTitle);
-            ImageView imageView = (ImageView) rootView.findViewById(R.id.fragment_detail_imageView_poster);
-            TextView textViewReleaseDate = (TextView) rootView.findViewById(R.id.fragment_detail_textView_releaseDate);
-            TextView textViewVoteAverage = (TextView) rootView.findViewById(R.id.fragment_detail_textView_voteAverage);
-            TextView textViewOverview = (TextView) rootView.findViewById(R.id.fragment_detail_textView_overview);
-
-            textViewOriginalTitle.setText(movie.getOriginalTitle());
-            textViewReleaseDate.setText(movie.getReleaseDate());
-            textViewVoteAverage.setText(movie.getVoteAverage());
-            textViewOverview.setText(movie.getOverview());
-
-            Picasso.with(getContext()).load("http://image.tmdb.org/t/p/w500" + movie.getPosterPath()).into(imageView);
             return rootView;
-        }*/
+
+        }
+
+        @Override
+        public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+            // Inflate the menu; this adds items to the action bar if it is present.
+            inflater.inflate(R.menu.detailfragment, menu);
+
+            // Retrieve the share menu item
+            MenuItem menuItem = menu.findItem(R.id.action_share);
+
+            // Get the provider and hold onto it to set/change the share intent.
+            mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+
+            // If onLoadFinished happens before this, we can go ahead and set the share intent now.
+            if (mMovieString != null) {
+                mShareActionProvider.setShareIntent(createShareMovieIntent());
+            }
+        }
+
+        private Intent createShareMovieIntent() {
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra(Intent.EXTRA_TEXT, mMovieString + MOVIE_SHARE_HASHTAG);
+            return shareIntent;
+        }
+
+        @Override
+        public void onActivityCreated(Bundle savedInstanceState) {
+            getLoaderManager().initLoader(DETAIL_LOADER, null, this);
+            super.onActivityCreated(savedInstanceState);
+        }
+
+
+        @Override
+        public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+            Log.v(LOG_TAG, "In onCreateLoader");
+            Intent intent = getActivity().getIntent();
+            if (intent == null) {
+                return null;
+            }
+
+            // Now create and return a CursorLoader that will take care of
+            // creating a Cursor for the data being displayed.
+            return new CursorLoader(
+                    getActivity(),
+                    intent.getData(),
+                    MOVIE_COLUMNS,
+                    null,
+                    null,
+                    null
+            );
+        }
+
+        @Override
+        public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+            Log.v(LOG_TAG, "In onLoadFinished");
+            if (!data.moveToFirst()) { return; }
+
+//            String dateString = Utility.formatDate(
+//                    data.getLong(COL_WEATHER_DATE));
+
+            String posterPath = data.getString(COL_POSTERPATH);
+            String originalTitle = data.getString(COL_ORIGINALTITLE);
+            String releaseDate = data.getString(COL_RELEASEDATE);
+            String overview = data.getString(COL_OVERVIEW);
+            String voteAverage = data.getString(COL_VOTEAVERAGE);
+            String id = data.getString(COL_ID_KEY);
+
+            TextView textViewOriginalTitle = (TextView) getView().findViewById(R.id.fragment_detail_textView_originalTitle);
+            ImageView imageView = (ImageView) getView().findViewById(R.id.fragment_detail_imageView_poster);
+            TextView textViewReleaseDate = (TextView) getView().findViewById(R.id.fragment_detail_textView_releaseDate);
+            TextView textViewVoteAverage = (TextView) getView().findViewById(R.id.fragment_detail_textView_voteAverage);
+            TextView textViewOverview = (TextView) getView().findViewById(R.id.fragment_detail_textView_overview);
+
+            textViewOriginalTitle.setText(originalTitle);
+            textViewReleaseDate.setText(releaseDate);
+            textViewVoteAverage.setText(voteAverage);
+            textViewOverview.setText(overview);
+
+            Picasso.with(getContext()).load("http://image.tmdb.org/t/p/w500" +
+                    posterPath).into(imageView);
+
+
+//
+//            boolean isMetric = Utility.isMetric(getActivity());
+//
+//            String high = Utility.formatTemperature(
+//                    data.getDouble(COL_WEATHER_MAX_TEMP), isMetric);
+//
+//            String low = Utility.formatTemperature(
+//                    data.getDouble(COL_WEATHER_MIN_TEMP), isMetric);
+//
+//            mForecast = String.format("%s - %s - %s/%s", dateString, weatherDescription, high, low);
+
+//            TextView detailTextView = (TextView)getView().findViewById(R.id.detail_text);
+//            detailTextView.setText(mForecast);
+
+            // If onCreateOptionsMenu has already happened, we need to update the share intent now.
+            if (mShareActionProvider != null) {
+                mShareActionProvider.setShareIntent(createShareMovieIntent());
+            }
+        }
+
+        @Override
+        public void onLoaderReset(Loader<Cursor> loader) {
+
+        }
     }
 }
--- app/src/main/java/com/example/wilbotiv/popularmoviess1/MoviesFragment.java
+++ app/src/main/java/com/example/wilbotiv/popularmoviess1/MoviesFragment.java
@@ -1,9 +1,12 @@
 package com.example.wilbotiv.popularmoviess1;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -11,11 +14,40 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.GridView;
 
-public class MoviesFragment extends Fragment implements LoaderManager.LoaderCallbacks{
+public class MoviesFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
 
     private MoviesAdapter mMoviesAdapter;
+//    private static final int MOVIE_LOADER = 0;
+
+    private static final String[] MOVIE_COLUMNS = {
+            // In this case the id needs to be fully qualified with a table name, since
+            // the content provider joins the location & weather tables in the background
+            // (both have an _id column)
+            // On the one hand, that's annoying.  On the other, you can search the weather table
+            // using the location set by the user, which is only in the Location table.
+            // So the convenience is worth it.
+            MovieContract.MovieEntry.TABLE_NAME + "." + MovieContract.MovieEntry._ID,
+            MovieContract.MovieEntry.COLUMN_POSTERPATH,
+            MovieContract.MovieEntry.COLUMN_ORIGINALTITLE,
+            MovieContract.MovieEntry.COLUMN_RELEASEDATE,
+            MovieContract.MovieEntry.COLUMN_OVERVIEW,
+            MovieContract.MovieEntry.COLUMN_ID_KEY,
+            MovieContract.MovieEntry.COLUMN_VOTEAVERAGE,
+    };
+
+    // These indices are tied to FORECAST_COLUMNS.  If FORECAST_COLUMNS changes, these
+    // must change.
+    static final int COL_MOVIE_ID = 0;
+    static final int COL_POSTERPATH = 1;
+    static final int COL_ORIGINALTITLE = 2;
+    static final int COL_RELEASEDATE = 3;
+    static final int COL_OVERVIEW = 4;
+    static final int COL_ID_KEY = 5;
+    static final int COL_VOTEAVERAGE = 6;
+
 
     private final int LOADER_ID = 0;
 
@@ -26,55 +58,6 @@ public class MoviesFragment extends Fragment implements LoaderManager.LoaderCall
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
-        getLoaderManager().initLoader(0, null, this);
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        updateMovie();
-    }
-
-    private void updateMovie() {
-        FetchMovieTask movieTask = new FetchMovieTask(getActivity());
-        /*SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
-        String location = prefs.getString(getString(R.string.pref_location_key),
-                getString(R.string.pref_location_default));*/
-//        weatherTask.execute(location);
-        movieTask.execute();
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-
-//        Uri moviesUri = MovieContract.MovieEntry.buildMovieUri();
-        Cursor cur = getActivity().getContentResolver().query(MovieContract.MovieEntry.CONTENT_URI, null, null, null, null);
-//        String[] projection = new String[]{BaseColumns._ID, UserDictionary.Words.WORD};
-//        Cursor cur = getActivity().getContentResolver().query(UserDictionary.Words.CONTENT_URI, projection, null, null, null);
-
-        mMoviesAdapter = new MoviesAdapter(getActivity(), cur, 0);
-
-        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
-        GridView gridView = (GridView) rootView.findViewById(R.id.gridview_movies);
-        gridView.setAdapter(mMoviesAdapter);
-//        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-/*
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-//                String movies = mMoviesAdapter.getItem(position).posterPath;
-//                Toast.makeText(getActivity(), movies, Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(getActivity(), DetailActivity.class);
-                Bundle bundle = new Bundle();
-                bundle.putParcelable(PAR_KEY, movies.get(position));
-                intent.putExtras(bundle);
-                startActivity(intent);
-            }
-        });
-*/
-        return rootView;
     }
 
     @Override
@@ -93,18 +76,81 @@ public class MoviesFragment extends Fragment implements LoaderManager.LoaderCall
     }
 
     @Override
-    public Loader onCreateLoader(int id, Bundle args) {
-        return null;
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+//        Uri moviesUri = MovieContract.MovieEntry.buildMovieUri();
+//        Cursor cur = getActivity().getContentResolver().query(MovieContract.MovieEntry.CONTENT_URI, null, null, null, null);
+//        String[] projection = new String[]{BaseColumns._ID, UserDictionary.Words.WORD};
+//        Cursor cur = getActivity().getContentResolver().query(UserDictionary.Words.CONTENT_URI, projection, null, null, null);
+
+        mMoviesAdapter = new MoviesAdapter(getActivity(), null, 0);
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        GridView gridView = (GridView) rootView.findViewById(R.id.gridview_movies);
+        gridView.setAdapter(mMoviesAdapter);
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Cursor cursor = (Cursor) parent.getItemAtPosition(position);
+                if (cursor != null) {
+                    Intent intent = new Intent(getActivity(), DetailActivity.class)
+                            .setData(MovieContract.MovieEntry.CONTENT_URI);
+                    startActivity(intent);
+                }
+            }
+        });
+
+/*
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+//                String movies = mMoviesAdapter.getItem(position).posterPath;
+//                Toast.makeText(getActivity(), movies, Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getActivity(), DetailActivity.class);
+                Bundle bundle = new Bundle();
+                bundle.putParcelable(PAR_KEY, movies.get(position));
+                intent.putExtras(bundle);
+                startActivity(intent);
+            }
+        });
+*/
+        return rootView;
+    }
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        getLoaderManager().initLoader(LOADER_ID, null, this);
+        super.onActivityCreated(savedInstanceState);
+    }
+
+
+    private void updateMovie() {
+        FetchMovieTask movieTask = new FetchMovieTask(getActivity());
+        /*SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        String location = prefs.getString(getString(R.string.pref_location_key),
+                getString(R.string.pref_location_default));*/
+//        weatherTask.execute(location);
+        movieTask.execute();
+    }
+
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        CursorLoader cur = new CursorLoader(getActivity(), MovieContract.MovieEntry.CONTENT_URI, null, null, null, null);
+//        CursorLoader cur = getActivity().getContentResolver().query(MovieContract.MovieEntry.CONTENT_URI, null, null, null, null);
+        return cur;
     }
 
     @Override
-    public void onLoadFinished(Loader loader, Object data) {
-
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        mMoviesAdapter.swapCursor(data);
     }
 
     @Override
-    public void onLoaderReset(Loader loader) {
-
+    public void onLoaderReset(Loader<Cursor> loader) {
+        mMoviesAdapter.swapCursor(null);
     }
 }
 
